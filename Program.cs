45d854d072023b845c61a29bd10b52ed2a7025d5
Gitou <sigitcold007@gmail.com>
2021-03-18 21:27:59 +0700

    Taxi Project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Pertemuan_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.name = "Jono";
+            taxi.duty = true;
+            taxi.num = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Pertemuan_3
+{
+    class Taxi
+    {
+        public string name { get; set; }
+        public bool duty { get; set; }
+        public float num { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", name);
+            Console.WriteLine("On Duty: {0}", duty);
+            Console.WriteLine("Number of Passenger: {0}\n", num);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} Sedang Menjemput Penumpang", name);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Sedang Menjemput Penumpang", name);
+        }
+    }
+}
